b024dd1eef4cae5daab61b06a49e3dc2234b0710
tRav3r <tRav3r@gmail.com>
2014-05-25 15:01:49 +0300

    TT changed, part_a - palindromes

--- main/palindrome.rb
+++ main/palindrome.rb
@@ -2,7 +2,7 @@ module Palindrome
   extend self
 
   def palindrome?(str)
-    prepared_str = str.downcase.gsub[/^a-z/], '')
+    prepared_str = str.downcase.gsub(/[^a-z]/, '')
 		prepared_str.reverse == prepared_str
 	end
 end
