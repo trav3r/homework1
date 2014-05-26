a69d55ea9e5790ddd95e032007d2754ff60976d7
tRav3r <tRav3r@gmail.com>
2014-05-26 20:53:03 +0300

    changed, part_b - word_count

--- main/word_count.rb
+++ main/word_count.rb
@@ -2,6 +2,7 @@ module Words
   extend self
 
   def count(str)
-    {}
+    result = str.downcase.gsub(/[^a-z ]/, '').split(/\s+/).group_by{|i| i}
+    result.each{|k,v| result[k] = v.count}
   end
 end
